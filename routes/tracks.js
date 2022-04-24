bb11b267fd18c1c4fad206c9949955c3606a6d06
chrxtian <18670933+chrxtian@users.noreply.github.com>
2022-04-24 18:52:52 -0500

    adding initial validation for track model

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const req = require('express/lib/request');
 const router = express.Router();
+const { validatorCreateItem } = require('../validators/tracks');
 const { getItems, getItem, createItem } = require("../controllers/tracks");
 
 router.get("/", getItems);
 router.get("/:id", getItem);
-router.post("/", createItem);
+router.post("/", validatorCreateItem, createItem);
 
 module.exports = router;
---
+++ utils/handleValitator.js
@@ -0,0 +1,17 @@
+const { validationResult } = require("express-validator");
+
+const validateResults = (req, res, next) => {
+    try {
+        console.warn('validatando...');
+        validationResult(req).throw()
+        console.warn('Validaton OK');
+        return next;
+    } catch (error) {
+        console.error(error);
+        res.status(400);
+        res.send({ errors: error.array() });
+        
+    }
+};
+
+module.exports =  validateResults;
