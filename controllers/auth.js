a82c847c6f2f0efec550c6851766ba834846b8c7
chrxtian <los9dragones@gmail.com>
2022-07-28 10:55:48 -0500

    finish implementation of login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,6 +38,28 @@ const registerController = async (req, res) => {
 const loginController = async (req, res) => {
   try {
     req = matchedData(req);
+    const user = await usersModel.findOne({email:req.email})
+      .select('password name role email');
+    if (!user){
+      handleHttpError(res, 'USER_NOT_EXIST', 404);
+      return;
+    }
+
+    const hashPassword = user.get('password');
+    const check = await compare(req.password, hashPassword);
+    if (!check){
+      handleHttpError(res, 'INVALID_CREDENTIALS', 401);
+      return;
+    }
+    user.set('password', undefined, {strict:false});
+
+    const data = {
+      token: await tokenSign(user),
+      user
+    };
+
+    res.send({data});
+
   } catch (error) {
     handleHttpError(res, 'ERROR_LOGIN_USER', 500)
   }
